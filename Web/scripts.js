37ce213b65f50e107c9c718beacd0952f94e7e23
unknown <annandaleweb@protonmail.com>
2023-07-23 00:19:28 -0700

    added image links and corrected minor issues.

--- Web/scripts.js
+++ Web/scripts.js
@@ -1,1 +1,22 @@
+< script >
+    document.getElementById('newsletter-form').addEventListener('submit', function (event) {
+        event.preventDefault();
 
+        var email = document.getElementById('email').value;
+
+        fetch('https://usX.api.mailchimp.com/3.0/lists/YOUR_LIST_ID/members', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'apikey YOUR_API_KEY'
+                },
+                body: JSON.stringify({
+                    email_address: email,
+                    status: 'subscribed'
+                })
+            })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }); <
+/script>
